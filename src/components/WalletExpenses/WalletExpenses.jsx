efab570b1b4da5dbd847aa0f83021c5e67771792
Priyanka Chaudhari <cpriyanka71m@gmail.com>
2025-08-13 18:03:36 +0530

    Fixed missing dependencies

--- src/components/WalletExpenses/WalletExpenses.jsx
+++ src/components/WalletExpenses/WalletExpenses.jsx
@@ -108,7 +108,7 @@ const WalletExpenses = ({
             <span className="income-amount"> ₹{walletBalance} </span>
           </h2>
           <button
-            className="glassmorphism "
+            className="glassmorphism"
             onClick={() => setIsIncomeModalOpen(true)}
           >
             + Add Income
@@ -120,7 +120,7 @@ const WalletExpenses = ({
             <span className="expense-amount"> ₹{getTotalExpenses()} </span>
           </h2>
           <button
-            className="glassmorphism "
+            className="glassmorphism"
             onClick={() => setIsExpenseModalOpen(true)}
           >
             + Add Expense
